66fe76e3d3b31795566d9b05707364d78ce99ba6
Erick Tejada <erick.tejada@tejadatech.com>
2024-09-27 20:18:23 -0400

    [IMP] migration to clear NCF fields for canceled bills

---
+++ l10n_do_accounting/upgrades/17.0.17.0/pre-01-account_moves.py
@@ -0,0 +1,21 @@
+import logging
+from odoo.upgrade import util
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    env = util.env(cr)
+
+    canceled_bills = env["account.move"].search([('state','=','cancel'),('move_type','=','in_invoice')])
+    names = []
+    for bill in canceled_bills:
+        names.append(bill.name)
+        bill.write({
+            'l10n_latam_document_number': '',
+            'l10n_do_fiscal_number': '',
+            'l10n_do_sequence_prefix': '',
+            'l10n_do_sequence_number': False,
+        })
+
+    _logger.info("Removed NCF info for: %s", ', '.join(names))
