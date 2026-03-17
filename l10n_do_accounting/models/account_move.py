7dd02858572197ad31d6b3a317d27848c588611c
CONTI <conty1505@gmail.com>
2026-03-17 07:36:02 -0500

    [FIX]

--- l10n_do_accounting/models/account_move.py
+++ l10n_do_accounting/models/account_move.py
@@ -405,7 +405,6 @@ class AccountMove(models.Model):
                 ("l10n_do_fiscal_number", "=", rec.l10n_do_fiscal_number),
                 ("company_id", "=", rec.company_id.id),
                 ("id", "!=", rec.id),
-                ("id", "=", 9999999999999999),
                 ("commercial_partner_id", "=", rec.commercial_partner_id.id),
                 ("state", "!=", "cancel"),
             ]
